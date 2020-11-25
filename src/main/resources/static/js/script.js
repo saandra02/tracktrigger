c8fc598cfac0d47f99412f25dc9d64c6109b732c
Saandra <saandra02@gmail.com>
2020-11-26 03:59:40 +0530

    Verification flow complete

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,25 @@
+var mini = true;
+
+function toggleSidebar() {
+  if (mini) {
+    console.log("opening sidebar");
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    this.mini = false;
+  } else {
+    console.log("closing sidebar");
+    document.getElementById("mySidebar").style.width = "85px";
+    document.getElementById("main").style.marginLeft = "85px";
+    this.mini = true;
+  }
+}
+
+function openWin() {
+  window.open("Signup.html");
+}
+
+function signoutWin(){
+  alert("You have logged out of your account!");
+  location.replace("Login.html");
+}
+
